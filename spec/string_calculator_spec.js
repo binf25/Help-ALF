f48b433045f54480fffc38624c34d424ddb402d2
Beyza <if24b040@technikum-wien.at>
2025-06-30 18:58:05 +0200

    Failing test for ., production code not needed

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -1,8 +1,12 @@
 describe("findSpaceship", function() {
-
+	
 	it("should return 'Spaceship lost forever.' for an empty map", function() {
 		// erwartet, dass Funktion existiert und diesen String zurückgibt
 		expect(findSpaceship("")).toEqual("Spaceship lost forever.");
 	});
-	
+
+	it("should return 'Spaceship lost forever.' for an empty map", function() {
+		expect(findSpaceship(".")).toEqual("Spaceship lost forever.");
+	});
+
 });
